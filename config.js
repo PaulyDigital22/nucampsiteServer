16c376a406f71ff80628b9085bd0498e97eeb990
Paul Carter <pcarter22@gmail.com>
2021-07-12 11:51:37 -0500

    Facebook Passport

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '2773868152737499',
+        clientSecret: '97e01d257f1a7596affa39c2a8039edc'
+    }
 }
